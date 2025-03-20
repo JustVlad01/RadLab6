3fdee64168e424a1716a1d7fda6d1305ed47db6f
JustVlad01 <s00233992@atu.ie>
2025-03-20 11:22:48 +0000

    Add project files.

---
+++ Rad302Week6Lab2025.DataModel.S00233992/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataModel
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public int ReorderLevel { get; set; }
+        public int ReorderQuantity { get; set; }
+        public double UnitPrice { get; set; }
+        public int StockOnHand { get; set; }
+    }
+}
