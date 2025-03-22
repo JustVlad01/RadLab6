c88a5b8de5313820c0fa125aaf1799a0124a2fc9
JustVlad01 <s00233992@atu.ie>
2025-03-22 12:12:05 +0000

    Week 6 Lab Supplier view setup S00233992

---
+++ RAD302Week6BlazorClient2025.S00233992/Pages/SupplierView.razor.cs
@@ -0,0 +1,49 @@
+﻿using DataModel;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using RAD302Week6BlazorClient2025.S00233992;
+
+namespace RAD302Week6BlazorClient2025.S00233992.Pages
+{
+    public partial class SupplierView
+    {
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        private Supplier[] suppliers;
+
+        protected override async Task OnInitializedAsync()
+        {
+            // Loading page for the first time
+            if (SupplierStaticContext.Suppliers == null)
+            {
+                SupplierStaticContext.Suppliers =
+                    suppliers = await Http.GetFromJsonAsync<Supplier[]>("sample-data/Supplier.json");
+                SupplierStaticContext.Products = await Http.GetFromJsonAsync<Product[]>("sample-data/Product.json");
+                InitialiseSupplierProducts();
+            }
+            else // We are reloading the page
+            {
+                suppliers = SupplierStaticContext.Suppliers;
+                await base.OnInitializedAsync();
+            }
+        }
+
+        private void InitialiseSupplierProducts()
+        {
+            foreach (var item in suppliers)
+            {
+                List<Product> toadd =
+                    SupplierStaticContext.Products.OrderBy(o => new Random().Next(2372))
+                    .Take(3).ToList();
+                item.SupplierProducts = new List<Product>();
+                item.SupplierProducts.AddRange(toadd);
+            }
+        }
+    } 
+}
